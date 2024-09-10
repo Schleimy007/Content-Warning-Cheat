385bfb8adf8c634ecd34b711833ce9277e5070c1
Reifensepp <108888172+DXXNS@users.noreply.github.com>
2024-09-10 09:28:25 +0200

    Removed old function

--- Watermark.cs
+++ Watermark.cs
@@ -9,7 +9,7 @@ namespace TestMod
 {
     public static class Watermark
     {
-        private static readonly System.Collections.Generic.List<string> activeConditions = new System.Collections.Generic.List<string>();
+        
         public static void Call()
         {
 
@@ -27,57 +27,6 @@ namespace TestMod
 
             GUI.Label(rect, fpsText, style);
             Rect rect1 = new Rect(0, 0 + 40, Screen.width, 100); // Adjust height as needed
-            UpdateUI();
-
-
-
         }
-        public static void UpdateUI()
-        {
-            activeConditions.Clear(); // Clear the list before updating it
-
-            // Check conditions and add to the list if true
-            if (Modules.infHeal)
-                activeConditions.Add("Infinite Heal");
-            if (Modules.infOxy)
-                activeConditions.Add("Infinite Oxygen");
-            if (Modules.infStam)
-                activeConditions.Add("Infinite Stamina");
-
-            if (Modules.teamESP)
-                activeConditions.Add("Team ESP");
-            if (Modules.mobESP)
-                activeConditions.Add("Mob ESP");
-            if (Modules.mobTracer)
-                activeConditions.Add("Mob Tracer");
-            if (Modules.divingBox)
-                activeConditions.Add("Diving Box");
-
-            if (Modules.killAll)
-                activeConditions.Add("Kill All");
-
-            if (Modules.infJump)
-                activeConditions.Add("Infinite Jump");
-            if (Modules.preventDeath)
-                activeConditions.Add("Prevent Death");
-
-            if (Modules.goodLight)
-                activeConditions.Add("Good Light");
-
-            // Display the active conditions on the screen
-            GUIStyle style = new GUIStyle(GUI.skin.label);
-            style.fontSize = 14;
-            style.alignment = TextAnchor.UpperLeft;
-            style.normal.textColor = Color.blue;
-
-            float yPos = 0;
-            foreach (string condition in activeConditions)
-            {
-                Rect rect = new Rect(0, yPos, 200, 20);
-                GUI.Label(rect, condition, style);
-                yPos += 20;
-            }
-        }
-
     }
 }
